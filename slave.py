5bc619c481a027e261258d34f84c03003a6c6fcf
Govind <thakurgovind1998@gmail.com>
2025-12-10 20:07:54 +0530

    class object

--- slave.py
+++ slave.py
@@ -6,3 +6,21 @@ Created on Wed Dec 10 19:53:22 2025
 """
 
 print("branch of git");
+
+
+    
+    
+# Define a class
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}")
+
+# Create an object of the class
+my_car = Car("BMW", "M4")
+
+# Call the method
+my_car.display_info()
